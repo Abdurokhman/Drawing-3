2af2145391b365c1ece7511a037750e543ea246e
Abdurokhman Mukhanbetov <muhanbetov07@icloud.com>
2022-10-20 19:17:27 +0400

    Add files via upload

---
+++ Drawing-3/Drawing-3/Drawing_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_3App.swift
+//  Drawing-3
+//
+//  Created by Abdurokhman Mukhanbetov on 20.10.22.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
